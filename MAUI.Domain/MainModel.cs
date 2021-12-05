2d2a68126744c7d472bdab6353a99dd8c2458848
Hayk Grigoryan <haykflexgrigoryan@gmail.com>
2021-12-05 19:10:15 +0400

    removed old project

--- MAUI.Domain/MainModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MAUI.Domain
-{
-    public class MainModel
-    {
-        public int TotalQuantity => SubItems.Sum(x => x.Quantity);
-        public IList<SubItem> SubItems { get; set; } = new List<SubItem>();
-    }
-}
