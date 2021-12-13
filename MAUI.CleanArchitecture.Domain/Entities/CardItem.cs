db02d8508c6cb3f4c09ad8f760f288b849f33db0
haykflexgrigoryan <haykflexgrigoryan@gmail.com>
2021-12-13 04:07:22 +0400

    added card item entity

---
+++ MAUI.CleanArchitecture.Domain/Entities/CardItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUI.CleanArchitecture.Domain.Entities
+{
+    public class CardItem
+    {
+        public int Quantity { get; set; }
+
+        public StoreItem StoreItem { get; set; } = new StoreItem();
+
+    }
+}
